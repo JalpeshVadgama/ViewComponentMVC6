409795bbee0aaca36ffb916fb1e04ea5c3614f1d
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-10-05 23:45:38 +0530

    Added view component in APP

---
+++ ViewComponentMVC/src/ViewComponentMVC/CategoryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using ViewComponentMVC.Models;
+
+namespace ViewComponentMVC
+{
+    [ViewComponent(Name = "ProductCategoriesComponent")]
+    public class CategoryViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+           var categories = new List<Category>()
+           {
+               new Category {CategoryId = 1, Name = "Clothes",Description = "Men and women's clothes"},
+               new Category {CategoryId = 2, Name = "Elentronics", Description = "Eletronics"}
+           };
+            return View(categories);
+        }
+
+    }
+}
