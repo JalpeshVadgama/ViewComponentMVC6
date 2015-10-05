e92ece397341a4cd3ec5388884b9633cc65ed164
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-10-05 23:24:21 +0530

    Initial Commit for View Component in MVC6

---
+++ ViewComponentMVC/src/ViewComponentMVC/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ViewComponentMVC.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
